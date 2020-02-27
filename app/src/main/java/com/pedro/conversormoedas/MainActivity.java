429a88b121145f8b6f0f3cb9a81f8cf02479ecce
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-02-27 17:17:18 -0300

    criando e identificando elementos

--- app/src/main/java/com/pedro/conversormoedas/MainActivity.java
+++ app/src/main/java/com/pedro/conversormoedas/MainActivity.java
@@ -3,12 +3,30 @@ package com.pedro.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ViewHolder
+        this.mViewHolder.editValue = findViewById(R.id.edit_valor);
+        this.mViewHolder.textDolar= findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+    }
+
+    private static class ViewHolder {
+    EditText editValue;
+    TextView textDolar;
+    TextView textEuro;
+    Button buttonCalculate;
     }
 }
