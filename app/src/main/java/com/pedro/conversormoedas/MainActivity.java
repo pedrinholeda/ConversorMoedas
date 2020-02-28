36a40f3b28882ce6fd746f15c6998046a32a168d
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-02-28 14:12:26 -0300

    Configurando evento de click e validando entrada de dados

--- app/src/main/java/com/pedro/conversormoedas/MainActivity.java
+++ app/src/main/java/com/pedro/conversormoedas/MainActivity.java
@@ -3,11 +3,13 @@ package com.pedro.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
 
@@ -21,6 +23,30 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textDolar= findViewById(R.id.text_dolar);
         this.mViewHolder.textEuro = findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+
+        this.clearValues();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button_calculate){
+            String value = this.mViewHolder.editValue.getText().toString();
+            if("".equals(value)) {
+                //mostrar mensagem pro usuario
+                Toast.makeText(this,this.getString( R.string.informe_valor),Toast.LENGTH_LONG).show();
+
+            } else {
+
+            }
+        }
+    }
+
+    private void clearValues(){
+        this.mViewHolder.textDolar.setText("");
+        this.mViewHolder.textEuro.setText("");
+
     }
 
     private static class ViewHolder {
